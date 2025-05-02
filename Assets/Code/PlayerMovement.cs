d86ea4327c09cfda21bf78ce11eee2036f7a79cf
Ahmet Tarik Duyar(Atduyar) <atduyar@gmail.com>
2025-05-02 15:11:31 +0200

    add: InputSystem and refactor: Movments \n\nDone https://trello.com/c/7i8M1FDf/12-improvement-variable-jump-mecanic

--- Assets/Code/PlayerMovement.cs
+++ Assets/Code/PlayerMovement.cs
@@ -1,11 +1,16 @@
 using UnityEngine;
+using UnityEngine.Animations;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Composites;
 
 public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody2D rb;
-    public float speed = 1000.0f;
-    public float jump = 1000.0f;
+    public float moveSpeed = 5.0f;
+    public float jumpPower = 10.0f;
     public float limit = 5.0f;
+    
+    private Vector2 movement;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -16,29 +21,49 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        rb.linearVelocityX /= 1.005f;
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            rb.AddForceX(-speed * Time.deltaTime);
-        }
-        else if (Input.GetKey(KeyCode.RightArrow))
-        {
-            rb.AddForceX(speed * Time.deltaTime);
-        }
+        rb.linearVelocity = new Vector2(movement.x * moveSpeed, rb.linearVelocity.y);
+        //rb.linearVelocityX /= 1.005f;
+        //if (Input.GetKey(KeyCode.LeftArrow))
+        //{
+        //    rb.AddForceX(-speed * Time.deltaTime);
+        //}
+        //else if (Input.GetKey(KeyCode.RightArrow))
+        //{
+        //    rb.AddForceX(speed * Time.deltaTime);
+        //}
 
-        if (Input.GetKeyDown(KeyCode.UpArrow))
-        {
-            rb.linearVelocityY = jump;
-        }
+        //if (Input.GetKeyDown(KeyCode.UpArrow))
+        //{
+        //    rb.linearVelocityY = jump;
+        //}
 
-        if (rb.linearVelocityX > limit)
-        {
-            rb.linearVelocityX = limit;
-        }
-        else if(rb.linearVelocityX < -limit)
-        {
-            rb.linearVelocityX = -limit;
-        }
+        //if (rb.linearVelocityX > limit)
+        //{
+        //    rb.linearVelocityX = limit;
+        //}
+        //else if(rb.linearVelocityX < -limit)
+        //{
+        //    rb.linearVelocityX = -limit;
+        //}
         //rb.AddForce(new Vector2(1,0));
+    }
+
+    public void Move(InputAction.CallbackContext ctx)
+    {
+        Debug.Log("Move");
+        movement.x = ctx.ReadValue<Vector2>().x;
+    }
+    public void Jump(InputAction.CallbackContext ctx)
+    {
+        if (ctx.performed)
+        {
+            Debug.Log("Jump");
+            rb.linearVelocityY = jumpPower;
+        }
+        else if(ctx.canceled)
+        {
+            Debug.Log("Jump Canceled");
+            rb.linearVelocityY *= 0.5f;
+        }
     }
 }

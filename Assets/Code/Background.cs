b75e78c398865be86c607a262e4d2af84254bff0
Abdullah <softwacar@gmail.com>
2025-05-02 14:09:43 +0200

    add : Parallax Background
    Complete https://trello.com/c/M2VxKmvI/7-level-1-fundation
    
    Co-Authored-By: Ahmet Tarık Duyar <info@atduyar.com>

---
+++ Assets/Code/Background.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class Background : MonoBehaviour
+{
+    private float startPos, lenght;
+    public GameObject cam;
+    public float parallaxEffect;
+    public float yOffset;
+    void Start()
+    {
+        startPos = transform.position.x;
+        lenght = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float distance = cam.transform.position.x * parallaxEffect;
+        float movement = cam.transform.position.x * (1 - parallaxEffect);
+
+        transform.position = new Vector3(startPos + distance, cam.transform.position.y + yOffset, transform.position.z);
+
+        if (movement > startPos + lenght)
+        {
+            startPos += lenght;
+        }
+        else if (movement < startPos - lenght)
+        {
+            startPos -= lenght;
+        }
+    }
+}

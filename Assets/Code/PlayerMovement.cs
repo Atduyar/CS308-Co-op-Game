1cca9f019a8bbf61e47a6cbc918db97bc358528b
Ahmet Tarık Duyar <atduyar@gmail.com>
2025-05-20 12:49:04 +0200

    feat: Fall variable gravity and double jump capability

--- Assets/Code/PlayerMovement.cs
+++ Assets/Code/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.Animations;
 using UnityEngine.InputSystem;
@@ -8,8 +9,12 @@ public class PlayerMovement : MonoBehaviour
     public Rigidbody2D rb;
     [Header("Movment")]
     public float moveSpeed = 5.0f;
-    public float jumpPower = 10.0f;
     private Vector2 movement;
+
+    [Header("Jump")]
+    public float jumpPower = 10.0f;
+    public int maxJump = 2;
+    public int jumpRemaining = 0;
 
     [Header("Sound")]
     public AudioClip jumpPlayer;
@@ -19,6 +24,10 @@ public class PlayerMovement : MonoBehaviour
     public Vector2 groundCheckSize = new Vector2(0.5f, 0.05f);
     public LayerMask groundLayer;
 
+    [Header("Gravity")]
+    public float baseGravity = 2f;
+    public float fallSpeedtMultiplier = 2f;
+    public float maxFallSpeed = 20f;
     void Start()
     {
         rb = this.GetComponent<Rigidbody2D>();
@@ -27,6 +36,21 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         rb.linearVelocity = new Vector2(movement.x * moveSpeed, rb.linearVelocity.y);
+        GroundedCheck();
+        Gravity();
+    }
+
+    public void Gravity()
+    {
+        if (rb.linearVelocity.y < 0)
+        {
+            rb.gravityScale = baseGravity * fallSpeedtMultiplier;
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, Mathf.Max(rb.linearVelocity.y, -maxFallSpeed));
+        }
+        else
+        {
+            rb.gravityScale = baseGravity;
+        }
     }
 
     public void Move(InputAction.CallbackContext ctx)
@@ -36,10 +60,11 @@ public class PlayerMovement : MonoBehaviour
 
     public void Jump(InputAction.CallbackContext ctx)
     {
-        if (ctx.performed && isGrounded())
+        if (ctx.performed && jumpRemaining > 0)
         {
             AudioSource.PlayClipAtPoint(jumpPlayer, transform.position);
             rb.linearVelocityY = jumpPower;
+            jumpRemaining--;
         }
         else if (ctx.canceled)
         {
@@ -48,14 +73,15 @@ public class PlayerMovement : MonoBehaviour
     }
 
 
-    public bool isGrounded()
+    public void GroundedCheck()
     {
-        if (Physics2D.OverlapBox(groundCheckPos.position, groundCheckSize, 0, groundLayer))
+        if (Physics2D.OverlapBox(groundCheckPos.position, groundCheckSize, 0, groundLayer)
+            && rb.linearVelocity.y <= 0)
         {
-            return true;
+            jumpRemaining = maxJump;
         }
-        return false;
     }
+
     private void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.white;

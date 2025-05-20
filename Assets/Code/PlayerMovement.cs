ac4d85f4e59541f7f463e7c9d5c72d10d93b119f
Ahmet Tarık Duyar <atduyar@gmail.com>
2025-05-20 11:55:07 +0200

    feat: Player jump grounded via collision box

--- Assets/Code/PlayerMovement.cs
+++ Assets/Code/PlayerMovement.cs
@@ -6,70 +6,59 @@ using UnityEngine.InputSystem.Composites;
 public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody2D rb;
+    [Header("Movment")]
     public float moveSpeed = 5.0f;
     public float jumpPower = 10.0f;
-    public float limit = 5.0f;
-
-    public AudioClip jumpPlayer;
-    
     private Vector2 movement;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [Header("Sound")]
+    public AudioClip jumpPlayer;
+    
+    [Header("GroundCheck")]
+    public Transform groundCheckPos;
+    public Vector2 groundCheckSize = new Vector2(0.5f, 0.05f);
+    public LayerMask groundLayer;
+
     void Start()
     {
         rb = this.GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         rb.linearVelocity = new Vector2(movement.x * moveSpeed, rb.linearVelocity.y);
-        //rb.linearVelocityX /= 1.005f;
-        //if (Input.GetKey(KeyCode.LeftArrow))
-        //{
-        //    rb.AddForceX(-speed * Time.deltaTime);
-        //}
-        //else if (Input.GetKey(KeyCode.RightArrow))
-        //{
-        //    rb.AddForceX(speed * Time.deltaTime);
-        //}
-
-        //if (Input.GetKeyDown(KeyCode.UpArrow))
-        //{
-        //    rb.linearVelocityY = jump;
-        //}
-
-        //if (rb.linearVelocityX > limit)
-        //{
-        //    rb.linearVelocityX = limit;
-        //}
-        //else if(rb.linearVelocityX < -limit)
-        //{
-        //    rb.linearVelocityX = -limit;
-        //}
-        //rb.AddForce(new Vector2(1,0));
     }
 
     public void Move(InputAction.CallbackContext ctx)
     {
-        //Debug.Log("Move");
         movement.x = ctx.ReadValue<Vector2>().x;
     }
 
     public void Jump(InputAction.CallbackContext ctx)
     {
-        if (ctx.performed)
+        if (ctx.performed && isGrounded())
         {
-            //Debug.Log("Jump");
+            AudioSource.PlayClipAtPoint(jumpPlayer, transform.position);
             rb.linearVelocityY = jumpPower;
         }
-        else if(ctx.canceled)
+        else if (ctx.canceled)
         {
-            //Debug.Log("Jump Canceled");
             rb.linearVelocityY *= 0.5f;
         }
+    }
 
-        AudioSource.PlayClipAtPoint(jumpPlayer, transform.position);
 
+    public bool isGrounded()
+    {
+        if (Physics2D.OverlapBox(groundCheckPos.position, groundCheckSize, 0, groundLayer))
+        {
+            return true;
+        }
+        return false;
+    }
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.white;
+        Gizmos.DrawCube(groundCheckPos.position, groundCheckSize);
     }
 }

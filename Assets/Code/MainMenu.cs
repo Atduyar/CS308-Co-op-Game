b69184c329fffd283bc29ee8fa4f0656e3b5f522
mustafaaokutaan <mokutan837@gmail.com>
2025-05-20 19:01:45 +0200

    feat: Main Menu Login And Lobi Panels
    Done https://trello.com/c/KXrYXtvg
    
    Co-Authored-By: Ahmet Tarık Duyar <info@atduyar.com>

--- Assets/Code/MainMenu.cs
+++ Assets/Code/MainMenu.cs
@@ -6,13 +6,22 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     public AudioClip selectSound;
+    public GameObject lobiPanel;
+    public GameObject loginPanel;
+    public GameObject creditsPanel;
+    public GameObject mainMenuPanel;
+
+
+    public void Start()
+    {
+        ShowMainMenu();
+    }
 
     public void StartGame()
     {
         AudioSource.PlayClipAtPoint(selectSound, transform.position);   
         int nextIndex = SceneManager.GetActiveScene().buildIndex + 1;
         SceneManager.LoadScene(nextIndex);
-
     }
 
     public void mainMenu()
@@ -20,18 +29,43 @@ public class MainMenu : MonoBehaviour
         AudioSource.PlayClipAtPoint(selectSound, transform.position);
         SceneManager.LoadScene("Buttons");
     }
-    public void settingsMenu()
-    {
-        AudioSource.PlayClipAtPoint(selectSound, transform.position);
-        SceneManager.LoadScene("SettingsMenu");
-
-    }
 
     public void QuitGame()
     {
-
         AudioSource.PlayClipAtPoint(selectSound, transform.position);
         Application.Quit();
+    }
+    public void ShowLoginPanel()
+    {
+        AudioSource.PlayClipAtPoint(selectSound, transform.position);
+        mainMenuPanel.SetActive(false);
+        loginPanel.SetActive(true);
+        lobiPanel.SetActive(false);
+        creditsPanel.SetActive(false);
+    }
 
+    public void ShowMainMenu()
+    {
+        AudioSource.PlayClipAtPoint(selectSound, transform.position);
+        mainMenuPanel.SetActive(true);
+        loginPanel.SetActive(false);
+        lobiPanel.SetActive(false);
+        creditsPanel.SetActive(false);
+    }
+    public void ShowCreditsMenu()
+    {
+        AudioSource.PlayClipAtPoint(selectSound, transform.position);
+        mainMenuPanel.SetActive(false);
+        loginPanel.SetActive(false);
+        lobiPanel.SetActive(false);
+        creditsPanel.SetActive(true);
+    }
+    public void ShowLobiMenu()
+    {
+        AudioSource.PlayClipAtPoint(selectSound, transform.position);
+        mainMenuPanel.SetActive(false);
+        loginPanel.SetActive(false);
+        lobiPanel.SetActive(true);
+        creditsPanel.SetActive(false);
     }
 }
